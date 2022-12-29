c588f5729c260709048ea7d019a649b2336ad098
Darya <daryakuzmich0@gmail.com>
2022-12-30 01:19:41 +0300

    Задание 60

---
+++ Task_60/Program.cs
@@ -0,0 +1,32 @@
+﻿Console.WriteLine("Введите высоту трехмерной матрицы");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите ширину трехмерной матрицы");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите глубину трехмерной матрицы");
+int h = Convert.ToInt32(Console.ReadLine());
+int[,,] array = new int[m,n,h];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        for (int k = 0; k < array.GetLength(2); k++)
+        {
+            array[i, j, k] = new Random().Next(10,100);
+        Console.Write("{0,10}" , + array[i, j, k]);
+        }
+        
+    }
+    Console.WriteLine();
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                Console.Write($"{array[i, j, k]} ({i},{j},{k}) ");
+            }
+            Console.WriteLine();
+            }
+    }    
